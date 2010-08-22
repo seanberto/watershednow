45072ef82c2925c9cf37496ca19b1705ebb8a17a
ThinkShout <sean@thinkshout.com>
2010-08-21 19:39:28 -0700

    Moving around modules/themes/library directories.

---
+++ sites/all/modules/contrib/webform/js/webform.js
@@ -0,0 +1,59 @@
+// $Id: webform.js,v 1.1.2.2 2010/04/11 06:13:51 quicksketch Exp $
+
+/**
+ * JavaScript behaviors for the front-end display of webforms.
+ */
+
+(function ($) {
+
+Drupal.behaviors.webform = function(context) {
+  // Calendar datepicker behavior.
+  Drupal.webform.datepicker(context);
+};
+
+Drupal.webform = Drupal.webform || {};
+
+Drupal.webform.datepicker = function(context) {
+  $('div.webform-datepicker').each(function() {
+    var $webformDatepicker = $(this);
+    var $calendar = $webformDatepicker.find('input.webform-calendar');
+    var startYear = $calendar[0].className.replace(/.*webform-calendar-start-(\d+).*/, '$1');
+    var endYear = $calendar[0].className.replace(/.*webform-calendar-end-(\d+).*/, '$1');
+
+    // Set up the jQuery datepicker element.
+    $calendar.datepicker({
+      dateFormat: 'yy-mm-dd',
+      yearRange: startYear + ':' + endYear,
+      onSelect: function(dateText, inst) {
+        var date = dateText.split('-');
+        $webformDatepicker.find('select.year, input.year').val(+date[0]);
+        $webformDatepicker.find('select.month').val(+date[1]);
+        $webformDatepicker.find('select.day').val(+date[2]);
+      },
+      beforeShow: function(input, inst) {
+        // Get the select list values.
+        var year = $webformDatepicker.find('select.year, input.year').val();
+        var month = $webformDatepicker.find('select.month').val();
+        var day = $webformDatepicker.find('select.day').val();
+
+        // If empty, default to the current year/month/day in the popup.
+        var today = new Date();
+        year = year ? year : today.getFullYear();
+        month = month ? month : today.getMonth() + 1;
+        day = day ? day : today.getDate();
+
+        // jQuery UI Datepicker will read the input field and base its date off
+        // of that, even though in our case the input field is a button.
+        $(input).val(year + '-' + month + '-' + day);
+      }
+    });
+
+    // Prevent the calendar button from submitting the form.
+    $calendar.click(function(event) {
+      $(this).focus();
+      event.preventDefault();
+    });
+  });
+}
+
+})(jQuery);
