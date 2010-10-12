30203edbae4ac804a0e63d893d4123e23e4adc1c
Troy Thompson <troynt@gmail.com>
2010-10-12 14:21:39 -0400

    theming search boxes.

--- drupal/sites/all/themes/watershed/template.php
+++ drupal/sites/all/themes/watershed/template.php
@@ -185,7 +185,7 @@ function watershed_preprocess_block(&$vars, $hook) {
         $subject = 'about us';
         break;
       case 'search_0':
-        $subject = '<none>';
+        $subject = 'search';
         break;
     }
     $block->subject = $subject;
@@ -411,39 +411,3 @@ function watershed_breadcrumb($breadcrumb) {
   }
 }
 
-/*
- * Override the default search form layout.
- */
-function watershed_preprocess_search_theme_form(&$vars, $hook) {
-
-  // Override the title field.
-  $vars['form']['search_theme_form']['#title'] = t('');
-
-  // Set a default value for the search box
-  $search_help = theme_get_setting('watershed_search_help');
-  $vars['form']['search_theme_form']['#value'] = $search_help;
-
-  // Add a custom class to the search box
-  $vars['form']['search_theme_form']['#attributes'] = array('class' => t('NormalTextBox txtSearch'),
-  'onfocus' => "if (this.value == '" . $search_help . "') {this.value = '';}",
-  'onblur' => "if (this.value == '') {this.value = '" . $search_help . "';}");
-
-// Change the text on the submit button
-  $button_text = theme_get_setting('watershed_search_button');
-  if ($button_text != '') {
-    $vars['form']['submit']['#value'] = $button_text;
-  } else {
-    $vars['form']['submit']['#value'] = t('Go');
-  }
-
-  // Rebuild the rendered version (search form only, rest remains unchanged)
-  unset($vars['form']['search_theme_form']['#printed']);
-  $vars['search']['search_theme_form'] = drupal_render($vars['form']['search_theme_form']);
-
-  // Rebuild the rendered version (submit button, rest remains unchanged)
-  unset($vars['form']['submit']['#printed']);
-  $vars['search']['submit'] = drupal_render($vars['form']['submit']);
-
-  // Collect all form elements to make it easier to print the whole form.
-  $vars['search_form'] = implode($vars['search']);
-}
---
+++ drupal/sites/all/themes/watershed/templates/search-block-form.tpl.php
@@ -0,0 +1,1 @@
+<?php include('search-theme-form.tpl.php'); ?>
