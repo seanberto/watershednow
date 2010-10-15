a28604476c269f739ee73a2b3fb101eb3da02027
ThinkShout <sean@thinkshout.com>
2010-10-15 14:45:09 -0700

    Tweaking profiler_install script to make sure that cookie jar is setup.

--- scripts/profiler_install.php
+++ scripts/profiler_install.php
@@ -2,66 +2,59 @@
 
 /**
  *
- * Just change different settings and run
- * php profiler-install.php
+ * This script is designed for commandline installation of the Watershed Now Drupal installation
+ * The script leverages cURL, and sets through the installation process with javacript disabled.
  *
  * Script will produce html for all urls visited. To collect the result use:
- * php profiler-install.php > drul.html
+ * php profiler-install.php > installation_steps.txt
  *  
  * Based upon the work of dennis iversen: http://www.os-cms.net/content/article/view/1
  */
 
-// your site
-$site_url = 'http://watershednow.com';
+// URL options
+$site_url = 'http://watershednow.com/';  // Site URL with trailing slash.
+$profile = 'watershednow'; // Name of selected install profile
+$locale = 'en'; // locale
+$tmpdir = '/tmp/'; // Temp directory on destination server with trailing slash.
 
-// profile
-$profile = 'watershednow';
+// URIs for installation steps
+/* @TODO - Uh, get rid of this hacky mess. These URLs were determined based upon manually parsing apache logs. */
+/* This is really fragile. Should probably hook into the installation profile itself. */
+$uris = array();
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile;
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile . '&op=start&id=1';
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile . '&id=1&op=do_nojs';
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile . '&id=1&op=finished';
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile;
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile . '&op=start&id=2';
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile . '&id=2&op=do_nojs';
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile . '&id=2&op=do_nojs';
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile . '&id=2&op=do_nojs';
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile . '&id=2&op=do_nojs';
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile . '&id=2&op=do_nojs';
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile . '&id=2&op=finished';
+$uris[] = 'install.php?locale=' . $locale . '&profile=' . $profile;
 
-// locale
-$locale = 'en';
+unset($_COOKIE['has_js']); // In case installing on a domain that's already set the javascript cookie. See: http://drupal.org/node/608826
 
-// end of settings
-
-// we need to do the same address again because cookie
-// is only set second time, then we get cookie and move on.
-
+// This the initial URI once to set up the Cookie Jar.
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_COOKIEJAR, "./cookieFileName");
-curl_setopt($ch, CURLOPT_URL,"$site_url/install.php?locale=$locale&profile=$profile");
-curl_setopt($ch, CURLOPT_POST, 1);
-$html = curl_exec ($ch); // execute the curl command
-curl_close ($ch);
-unset($ch);
-echo $html;
-
-// install modules part one 80 %
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_COOKIEFILE, "./cookieFileName");
-curl_setopt($ch, CURLOPT_URL,"$site_url/install.php?profile=$profile&locale=$locale&op=do_nojs&id=1");
-$html = curl_exec ($ch); // execute the curl command
-curl_close ($ch);
-unset($ch);
-echo $html;
-
-// install last 20 %
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_COOKIEFILE, "./cookieFileName");
-curl_setopt($ch, CURLOPT_URL,"$site_url/install.php?profile=$profile&locale=$locale&op=do_nojs&id=2");
-$html = curl_exec ($ch); // execute the curl command
-curl_close ($ch);
-unset($ch);
-echo $html;
-   
-// enter these settings in a last curl session
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_COOKIEFILE, "./cookieFileName");
-curl_setopt($ch, CURLOPT_URL,"$site_url/install.php?profile=$profile&locale=$locale");
+curl_setopt($ch, CURLOPT_COOKIEJAR, $tmpdir . "cookieFileName");
+curl_setopt($ch, CURLOPT_URL, $site_url . 'install.php?locale=' . $locale . '&profile=' . $profile);
 $html = curl_exec ($ch);
 curl_close ($ch);
 echo $html;
+
+// Then pass through all the installation steps listed above.
+foreach ($uris as $uri) {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+  curl_setopt($ch, CURLOPT_COOKIEFILE, $tmpdir . "cookieFileName");
+  curl_setopt($ch, CURLOPT_URL, $site_url . $uri);
+  $html = curl_exec ($ch);
+  curl_close ($ch);
+  echo $html;
+}
 
 ?>
