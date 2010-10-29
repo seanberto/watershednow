da5dee9c002000ccc7938c5d7f605caeb9dddd66
Troy Thompson <troynt@gmail.com>
2010-10-29 18:01:13 -0400

    adding half, full specific classes for callout. only show pager on half slide style. commenting the building of slides

--- drupal/sites/all/modules/features/wn_hp_callout/js/wn_hp_callout.js
+++ drupal/sites/all/modules/features/wn_hp_callout/js/wn_hp_callout.js
@@ -2,11 +2,16 @@ jQuery(function(){
   var $ = jQuery;
   $('.wn-hp-callout').each(function(){
     var $t = $(this);
-    var pager_id = 'wn-hp-callout-pager-'+ Math.floor(Math.random()*99999);
-    $('<div id="' + pager_id + '" class="wn-hp-callout-pager" />').appendTo($t);
-    $t.find('ul').cycle({
-      'pager': '#'+pager_id, //can't pass a object, so generated random id
+    var cycle_opts = {
       'timeout':8000
-    })
+    }
+
+    //only display the pager if we are using half slide style 
+    if( $t.hasClass('wn-hp-callout-half') ) {
+      var pager_id = 'wn-hp-callout-pager-'+ Math.floor(Math.random()*99999);//generating random id
+      $('<div id="' + pager_id + '" class="wn-hp-callout-pager" />').appendTo($t);
+      cycle_opts['pager'] = '#'+pager_id; //can't pass a object, so generated random id
+    }
+    $t.find('ul').cycle(cycle_opts);
   })
 });
